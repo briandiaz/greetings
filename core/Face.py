683fb2443f0d6f56b6eeec6742facdda4ecd444d
Brian Díaz <brianediaz7@gmail.com>
2021-10-09 02:54:33 -0400

    facial-recognition/initial: Add FaceDetection and Face classes.
    facial-recognition/initial: Find faces and draw bounding boxes into image.

---
+++ core/Face.py
@@ -0,0 +1,10 @@
+
+class Face:
+  def __init__(self, id, score, boundingBox):
+    self.id = id
+    self.score = score
+    self.boundingBox = boundingBox
+
+  def __str__(self) -> str:
+    return "Face #%d with %.1f%% at %s" % (self.id, self.score * 100, self.boundingBox)
+
---
+++ core/FaceDetection.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+from core.Face import Face
+
+class FaceDetection():
+  def __init__(self, minDetectionConfidence=0.5):
+    self.minDetectionConfidence = minDetectionConfidence
+    self.faceDetection = mp.solutions.face_detection.FaceDetection(self.minDetectionConfidence)
+
+  def findFaces(self, image):
+    self.image = image
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    detectionProcess = self.faceDetection.process(imageRGB)
+    faces = []
+
+    if detectionProcess.detections:
+      for index, detection in enumerate(detectionProcess.detections):
+        boundingBoxLocation = detection.location_data.relative_bounding_box
+        imageHeight, imageWidth, _ = image.shape
+
+        if boundingBoxLocation.xmin > 0:
+          boundingBox = int(boundingBoxLocation.xmin * imageWidth), int(boundingBoxLocation.ymin * imageHeight), \
+                int(boundingBoxLocation.width * imageWidth), int(boundingBoxLocation.height * imageHeight)
+
+          face = Face(index + 1, detection.score[0], boundingBox)
+
+          faces.append(face)
+
+    return faces
+
+  def draw(self, faces):
+    lineWidth = 3
+
+    for _, face in enumerate(faces):
+      print(face)
+      x, y, w, h = face.boundingBox
+      x1, y1 = x + w, y + h
+      cv2.rectangle(self.image, (x1, y), (x, y1), (255, 0, 255), lineWidth)
+ 
